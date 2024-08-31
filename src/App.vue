77be97647a23ec2e3b604e32e063e75840f081fd
Kenneth Terrago <kterrago091893@gmail.com>
2024-08-31 23:14:29 +0800

    Multiple Components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
+  <Modal/>
 </template>
 
 <script>
+import Modal from './components/Modal.vue';
+
 export default {
   name: 'App',
   data() {
@@ -12,14 +13,9 @@ export default {
       title: 'My First Vue App :)'
     }
   },
-  methods: {
-    handleClick() {
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
-    }
+  components: {
+    Modal
   }
-
 }
 </script>
 
