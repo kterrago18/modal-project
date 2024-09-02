6ae39e5bee74cd3628debac127d4d9173f6aa82f
Kenneth Terrago <kterrago091893@gmail.com>
2024-09-03 00:45:51 +0800

    Passing Data with Props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal/>
+  <Modal :header="header" :text="text" theme="sale"/>
 </template>
 
 <script>
@@ -10,7 +10,9 @@ export default {
   name: 'App',
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price!'
     }
   },
   components: {
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="backdrop">
-        <div class="modal">
-            <h1>Modal Title</h1>
-            <p>modal content</p>
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: ['header', 'text', 'theme']
+}
+</script>
 
 <style scoped>
 .modal {
@@ -15,6 +21,7 @@
     background: white;
     border-radius: 10px;
 }
+
 .backdrop {
     top: 0;
     position: fixed;
@@ -22,7 +29,17 @@
     width: 100%;
     height: 100%;
 }
+
 h1 {
     color: green;
+}
+
+.modal.sale {
+    background: crimson;
+    color: white;
+}
+
+.modal.sale h1 {
+    color: white;
 }
 </style>g
